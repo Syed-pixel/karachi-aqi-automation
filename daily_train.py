c179e3b76cf56ddfb09c1520b66416335acb746b
Syed-pixel <122589751+Syed-pixel@users.noreply.github.com>
2026-01-24 21:21:41 +0500

    Update HF_TOKEN for authentication

--- daily_train.py
+++ daily_train.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_absolute_error, r2_score
 import xgboost as xgb
 from datetime import datetime
 
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 REPO_ID = "Syed110-3/karachi-aqi-predictor"
 login(token=HF_TOKEN)
 
